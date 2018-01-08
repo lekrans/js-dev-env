293e0838f95fc25caad61c899ab3f1cb2179c791
Michael Lekrans <michael.lekrans@chyronhego.com>
2018-01-08 09:25:23 +0100

    test

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const open = require('open');
+
+const port = 3000;
+const app = express();
+
+app.get('/', function(req, res) {
+  //res.sendFile()
+})
